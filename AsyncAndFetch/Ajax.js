0f9f9a1913ac67bf9fa90f9c85fd9ff3109fd263
Akshay Kannan <akshaykannan9677@gmail.com>
2020-07-14 21:54:00 +0530

    Test Cases for Fetch And Ajax

--- AsyncAndFetch/Ajax.js
+++ AsyncAndFetch/Ajax.js
@@ -1,117 +1,142 @@
-//-------GET REQUEST------
-var fetching = async function () {
-  //GET Request using AJAX
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      var k = document.querySelector("div");
-      k.innerHTML = http.response;
-    }
-  };
-  http.open("GET", "https://httpbin.org/html", true);
-  http.send();
-
-  //GET Request using Fetch
-  fetch("https://jsonplaceholder.typicode.com/posts/1").then(function (
-    response
-  ) {
-    response.json().then(function (data) {
-      var k = document.querySelector("h1");
-
-      k.innerHTML = data.title;
-    });
-  });
-};
-
-//-------POST REQUEST------
-var sendData = async function () {
-  //   POST Request using Fetch
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify({
-      title: "Akshay",
-      body: "AKSHAY KANNAN",
-      userId: 1,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
+var obj = {
+  //-------GET REQUEST------
+  getRequest: {
+    getAjax: async function () {
+      //GET Request using AJAX
+      var http = new XMLHttpRequest();
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          var x = JSON.parse(http.response);
+          var k = document.querySelector("div");
+          var v = document.querySelector("h1");
+          v.innerHTML = x.title;
+          k.innerHTML = x.body;
+          return x.body;
+        }
+      };
+      http.open("GET", "https://jsonplaceholder.typicode.com/posts/1", true);
+      http.send();
     },
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      console.log(json, "POST Request using Fetch ");
-    });
+    getFetch: async function () {
+      //GET Request using Fetch
+      var x;
+      return fetch("https://jsonplaceholder.typicode.com/posts/1").then(
+        function (response) {
+          response.json().then(function (data) {
+            var k = document.querySelector("h1");
 
-  //POST request using AJAX
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 201) {
-      var a = http.response;
-      var b = JSON.parse(a);
-      console.log(b, "POST request using AJAX");
-    }
-  };
-  http.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
-  http.setRequestHeader("Content-Type", "application/json");
-  http.send(
-    JSON.stringify({
-      title: "Akshay",
-      body: "AKSHAY KANNAN",
-      userId: 1,
-    })
-  );
-  alert("Data send");
-};
-
-//-------PUT REQUEST------
-var updateData = async function () {
-  //PUT Request using Fetch API
-  fetch("https://jsonplaceholder.typicode.com/posts/1", {
-    method: "PUT",
-    body: JSON.stringify({
-      id: 1,
-      title: "Akshay",
-      body: "Akshay Kanann",
-      userId: 1,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
+            k.innerHTML = data.title;
+          });
+        }
+      );
     },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json, "PUT Request using Fetch API"));
-
-  //PUT Request with AJAX
-  var http = new XMLHttpRequest();
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      var a = http.response;
-      var b = JSON.parse(a);
-      console.log(b, "PUT Request with AJAX");
-    }
-  };
-  http.open("PUT", "https://jsonplaceholder.typicode.com/posts/1", true);
-  http.setRequestHeader("Content-Type", "application/json");
-  http.send(
-    JSON.stringify({
-      id: 1,
-      title: "Akshay",
-      body: "Akshay Kanann",
-      userId: 1,
-    })
-  );
+  },
+  postRequest: {
+    //-------POST REQUEST------
+    postAjax: async function () {
+      //POST request using AJAX
+      var http = new XMLHttpRequest();
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 201) {
+          var a = http.response;
+          var b = JSON.parse(a);
+          return b;
+        }
+      };
+      http.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+      http.setRequestHeader("Content-Type", "application/json");
+      http.send(
+        JSON.stringify({
+          title: "Akshay",
+          body: "AKSHAY KANNAN",
+          userId: 1,
+        })
+      );
+    },
+    postFetch: async function () {
+      //   POST Request using Fetch
+      return fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title: "Akshay",
+          body: "AKSHAY KANNAN",
+          userId: 1,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          return json;
+        });
+    },
+  },
+  //-------PUT REQUEST------
+  putRequest: {
+    putAjax: async function () {
+      //PUT Request with AJAX
+      var http = new XMLHttpRequest();
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          var a = http.response;
+          var b = JSON.parse(a);
+          console.log(b);
+          return b;
+        }
+      };
+      http.open("PUT", "https://jsonplaceholder.typicode.com/posts/1", true);
+      http.setRequestHeader("Content-Type", "application/json");
+      http.send(
+        JSON.stringify({
+          id: 1,
+          title: "Akshay",
+          body: "Akshay Kanann",
+          userId: 1,
+        })
+      );
+    },
+    putFetch: async function () {
+      //PUT Request using Fetch API
+      fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "PUT",
+        body: JSON.stringify({
+          id: 1,
+          title: "Akshay",
+          body: "Akshay Kanann",
+          userId: 1,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => json);
+    },
+  },
+  deleteRequest: {
+    deleteAjax: async function () {
+      //DELETE Request using AJAX
+      var http = new XMLHttpRequest();
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          console.log("Data Deleted");
+          return true;
+        }
+      };
+      http.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
+      http.send();
+    },
+    deleteFetch: async function () {
+      //DELETE Request using Fetch API
+      fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "DELETE",
+      }).then(() => true);
+    },
+  },
 };
 
-//-------DELETE REQUEST------
-var deleteData = async function () {
-  //DELETE Request using Fetch API
-  fetch("https://jsonplaceholder.typicode.com/posts/1", {
-    method: "DELETE",
-  });
+//DELETE Request
 
-  //DELETE Request using AJAX
-  var http = new XMLHttpRequest();
-  http.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
-  http.send();
-  alert("Deleted Data");
-};
+module.exports = obj;
---
+++ AsyncAndFetch/Ajax.test.js
@@ -0,0 +1,76 @@
+const obj = require("./Ajax");
+describe("Test cases for CRUD Operations using Fetch API", () => {
+  test("GET Request with Fetch", () => {
+    obj.getRequest.getFetch().then((title) => {
+      expect(title).toBe(
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+      );
+    });
+  });
+  test("GET Request with AJAX", () => {
+    obj.getRequest.getAjax().then((body) => {
+      expect(body).toBe(
+        "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+      );
+    });
+  });
+  test("POST Request with Fetch", () => {
+    var expected = {
+      title: "Akshay",
+      body: "AKSHAY KANNAN",
+      userId: 1,
+      id: 101,
+    };
+    obj.postRequest.postFetch().then((data) => {
+      expect(data).toBe(expected);
+    });
+  });
+  test("POST Request with AJAX", () => {
+    var expected = {
+      title: "Akshay",
+      body: "AKSHAY KANNAN",
+      userId: 1,
+      id: 101,
+    };
+    obj.postRequest.postAjax().then((data) => {
+      expect(data).toBe(expected);
+    });
+  });
+  test("PUT Request using Fetch", () => {
+    var expected = { id: 1, title: "Akshay", body: "Akshay Kanann", userId: 1 };
+    obj.putRequest.putFetch().then((data) => {
+      expect(data).toBe(expected);
+    });
+  });
+  test("PUT Request using AJAX", () => {
+    var expected = { id: 1, title: "Akshay", body: "Akshay Kanann", userId: 1 };
+    obj.putRequest.putAjax().then((data) => {
+      expect(data).toBe(expected);
+    });
+  });
+  test("DELETE Request using Fetch", () => {
+    obj.deleteRequest.deleteFetch().then((data) => {
+      expect(data).toBeTruthy();
+    });
+  });
+  test("DELETE Request using AJAX", () => {
+    obj.deleteRequest.deleteAjax().then((data) => {
+      expect(data).toBeTruthy();
+    });
+  });
+});
+
+//--------Test Cases Result---------
+// PASS  ./Ajax.test.js
+// Test cases for CRUD Operations using Fetch API
+//   √ GET Request with Fetch (1 ms)
+//   √ GET Request with AJAX (17 ms)
+//   √ POST Request with Fetch (1 ms)
+//   √ POST Request with AJAX (4 ms)
+//   √ PUT Request using Fetch
+//   √ PUT Request using AJAX (3 ms)
+//   √ DELETE Request using Fetch
+//   √ DELETE Request using AJAX (3 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       8 passed, 8 total
